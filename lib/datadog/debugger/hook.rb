b0b7deade5bfee8ef11b20bfdd950cb2845c749a
Oleg Pudeyev <code@olegp.name>
2024-05-10 11:17:29 -0400

    di hooking

---
+++ lib/datadog/debugger/hook.rb
@@ -0,0 +1,80 @@
+require 'concurrent-ruby'
+require 'byebug'
+
+module Datadog
+  module Debugger
+    module Hook
+      module_function def clear_hooks
+        INSTRUMENTED_METHODS.clear
+        INSTRUMENTED_LINES.clear
+
+        TRACEPOINT_MUTEX.synchronize do
+          @tracepoint&.disable
+        end
+      end
+
+      module_function def hook_method(cls_name, meth_name)
+        cls = symbolize_class_name(cls_name)
+        id = next_id
+
+        cls.class_eval do
+          saved = instance_method(meth_name)
+
+          remove_method(meth_name)
+          define_method(meth_name) do |*args, **kwargs|
+            if INSTRUMENTED_METHODS[[cls_name, meth_name]] == id
+              saved.bind(self).call(*args, **kwargs).tap do |rv|
+                yield rv: rv
+              end
+            else
+              saved.bind(self).call(*args, **kwargs)
+            end
+          end
+        end
+
+        INSTRUMENTED_METHODS[[cls_name, meth_name]] = id
+      end
+
+      module_function def hook_line(file, line_no, &block)
+        # TODO is file a basename, path suffix or full path?
+        INSTRUMENTED_LINES[line_no] ||= {}
+        INSTRUMENTED_LINES[line_no][file] = block
+        p '--'
+        pp INSTRUMENTED_LINES.each.to_a
+
+        TRACEPOINT_MUTEX.synchronize do
+          $tracepoint ||= TracePoint.new(:line) do |tp|
+            on_line_tracepoint(tp)
+          end
+
+          $tracepoint.enable
+        end
+      end
+
+      private
+
+      INSTRUMENTED_METHODS = Concurrent::Map.new
+      INSTRUMENTED_LINES = Concurrent::Map.new
+      NEXT_MUTEX = Mutex.new
+      TRACEPOINT_MUTEX = Mutex.new
+
+      module_function def next_id
+        NEXT_MUTEX.synchronize do
+          @next_id ||= 0
+          @next_id += 1
+        end
+      end
+
+      module_function def symbolize_class_name(cls_name)
+        Object.const_get(cls_name)
+      end
+
+      module_function def on_line_tracepoint(tp)
+        cb = INSTRUMENTED_LINES[tp.lineno]&.[](File.basename(tp.path))
+        #p tp.object_id
+        #p tp if cb
+        cb&.call(tp)
+      end
+    end
+  end
+end
---
+++ spec/datadog/debugger/hook_spec.rb
@@ -0,0 +1,122 @@
+#require 'spec_helper'
+require 'datadog/debugger/hook'
+require_relative 'hook_line'
+
+Warning.ignore(/void context/, /spec/)
+RSpec.configure do |config|
+  config.expose_dsl_globally = true
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = [:should, :expect]
+  end
+end
+
+class HookTestClass
+  def hook_test_method
+    42
+  end
+
+  def hook_test_method_with_arg(arg)
+    arg
+  end
+
+  def hook_test_method_with_kwarg(kwarg:)
+    kwarg
+  end
+end
+
+describe Datadog::Debugger::Hook do
+  let(:observed_calls) { [] }
+
+  after do
+    described_class.clear_hooks
+  end
+
+  describe '.hook_method' do
+    context 'no args' do
+      it 'invokes callback' do
+        described_class.hook_method(:HookTestClass, :hook_test_method) do |payload|
+          observed_calls << payload
+        end
+
+        HookTestClass.new.hook_test_method.should == 42
+
+        observed_calls.length.should == 1
+        observed_calls.first.should == {rv: 42}
+      end
+    end
+
+    context 'positional args' do
+      it 'invokes callback' do
+        described_class.hook_method(:HookTestClass, :hook_test_method_with_arg) do |payload|
+          observed_calls << payload
+        end
+
+        HookTestClass.new.hook_test_method_with_arg(2).should == 2
+
+        observed_calls.length.should == 1
+        observed_calls.first.should == {rv: 2}
+      end
+    end
+
+    context 'when hooked twice' do
+      it 'only invokes callback once' do
+        described_class.hook_method(:HookTestClass, :hook_test_method) do |payload|
+          observed_calls << payload
+        end
+
+        described_class.hook_method(:HookTestClass, :hook_test_method) do |payload|
+          observed_calls << payload
+        end
+
+        HookTestClass.new.hook_test_method.should == 42
+
+        observed_calls.length.should == 1
+        observed_calls.first.should == {rv: 42}
+      end
+    end
+  end
+
+  describe '.hook_line' do
+    context 'method definition line' do
+      it 'does not invoke callback' do
+        described_class.hook_line('hook_line.rb', 2) do |payload|
+          observed_calls << payload
+        end
+
+        HookLineTestClass.new.test_method
+
+        observed_calls.should be_empty
+      end
+    end
+
+    context 'line inside of method' do
+      it 'invokes callback' do
+        described_class.hook_line('hook_line.rb', 3) do |payload|
+          observed_calls << payload
+        end
+
+        HookLineTestClass.new.test_method
+
+        observed_calls.length.should == 1
+        observed_calls.first.should be_a(TracePoint)
+      end
+    end
+
+    context 'when hooked twice' do
+      xit 'invokes callback only once' do
+        described_class.hook_line('hook_line.rb', 3) do |payload|
+          observed_calls << payload
+        end
+
+        described_class.hook_line('hook_line.rb', 3) do |payload|
+          observed_calls << payload
+        end
+
+        HookLineTestClass.new.test_method
+
+        observed_calls.length.should == 1
+        observed_calls.first.should be_a(TracePoint)
+      end
+    end
+  end
+end
