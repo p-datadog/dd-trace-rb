1455596bd3a2b6a2d6fca8b6a9f5201d468a08d7
Oleg Pudeyev <code@olegp.name>
2024-05-10 14:07:16 -0400

    cap test

--- spec/datadog/core/remote/client/capabilities_spec.rb
+++ spec/datadog/core/remote/client/capabilities_spec.rb
@@ -82,6 +82,74 @@ RSpec.describe Datadog::Core::Remote::Client::Capabilities do
     end
   end
 
+  context 'Debugger component' do
+    context 'when disabled' do
+      let(:settings) do
+        settings = Datadog::Core::Configuration::Settings.new
+        settings.debugger.enabled = false
+        settings
+      end
+
+      it 'does not register any capabilities, products, and receivers' do
+        expect(capabilities.capabilities).to_not include(4)
+        expect(capabilities.products).to_not include('ASM')
+        expect(capabilities.receivers).to_not include(
+          lambda { |r|
+            r.match? Datadog::Core::Remote::Configuration::Path.parse('datadog/1/DEBUGGER/_/_')
+          }
+        )
+      end
+
+      describe '#base64_capabilities' do
+        it 'matches tracing capabilities only' do
+          expect(capabilities.base64_capabilities).to eq('IAAAAA==')
+        end
+      end
+    end
+
+    context 'when not present' do
+      it 'does not register any capabilities, products, and receivers' do
+        expect(capabilities.capabilities).to_not include(4)
+        expect(capabilities.products).to_not include('ASM')
+        expect(capabilities.receivers).to_not include(
+          lambda { |r|
+            r.match? Datadog::Core::Remote::Configuration::Path.parse('datadog/1/DEBUGGER/_/_')
+          }
+        )
+      end
+
+      describe '#base64_capabilities' do
+        it 'matches tracing capabilities only' do
+          expect(capabilities.base64_capabilities).to eq('IAAAAA==')
+        end
+      end
+    end
+
+    context 'when enabled' do
+      let(:settings) do
+        settings = Datadog::Core::Configuration::Settings.new
+        settings.debugger.enabled = true
+        settings
+      end
+
+      it 'register capabilities, products, and receivers' do
+        expect(capabilities.capabilities).to include(4)
+        expect(capabilities.products).to include('ASM')
+        expect(capabilities.receivers).to include(
+          lambda { |r|
+            r.match? Datadog::Core::Remote::Configuration::Path.parse('datadog/1/DEBUGGER/_/_')
+          }
+        )
+      end
+
+      describe '#base64_capabilities' do
+        it 'returns binary capabilities' do
+          expect(capabilities.base64_capabilities).to_not be_empty
+        end
+      end
+    end
+  end
+
   context 'Tracing component' do
     it 'register capabilities, products, and receivers' do
       expect(capabilities.capabilities).to eq([1 << 29])
