762dfefba7656b09b147960743277cc7efa7d551
Oleg Pudeyev <code@olegp.name>
2024-05-14 15:07:51 -0400

    debugger remote

---
+++ lib/datadog/debugging/remote.rb
@@ -0,0 +1,64 @@
+module Datadog
+  module Debugging
+    module Remote
+      class ReadError < StandardError; end
+
+      class << self
+        PRODUCT = 'LIVE_DEBUGGING'
+
+        def products
+          [PRODUCT]
+        end
+
+        def capabilities
+          []
+        end
+
+        def process_config(config, content)
+          # content is one probe info
+          component = Datadog.send(:components).debugging
+          component.add_probe_from_remote(config)
+
+          content.applied
+
+#          Datadog.send(:components).telemetry.client_configuration_change!(env_vars)
+        rescue => e
+        raise
+          content.errored("#{e.class.name} #{e.message}: #{Array(e.backtrace).join("\n")}")
+        end
+
+        def receivers
+          receiver do |repository, _changes|
+            # DEV: Filter our by product. Given it will be very common
+            # DEV: we can filter this out before we receive the data in this method.
+            # DEV: Apply this refactor to AppSec as well if implemented.
+            repository.contents.map do |content|
+              case content.path.product
+              when PRODUCT
+                config = parse_content(content)
+                process_config(config, content)
+              end
+            end
+          end
+        end
+
+        def receiver(products = [PRODUCT], &block)
+          matcher = Core::Remote::Dispatcher::Matcher::Product.new(products)
+          [Core::Remote::Dispatcher::Receiver.new(matcher, &block)]
+        end
+
+        private
+
+        def parse_content(content)
+          data = content.data.read
+
+          content.data.rewind
+
+          raise ReadError, 'EOF reached' if data.nil?
+
+          JSON.parse(data)
+        end
+      end
+    end
+  end
+end
