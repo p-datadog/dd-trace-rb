ba981853268f8c3189fe34acd652f24771131591
Oleg Pudeyev <code@olegp.name>
2024-05-20 12:24:56 -0400

    hook - require

---
+++ lib/datadog/debugging.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative 'debugging/component'
+require_relative 'debugging/configuration'
+require_relative 'debugging/extensions'
+require_relative 'debugging/hook'
+require_relative 'debugging/remote'
+
+module Datadog
+  # Namespace for Datadog debugging instrumentation
+  module Debugging
+    class << self
+      def enabled?
+        Datadog.configuration.debugging.enabled
+      end
+    end
+
+    # Expose debugging to global shared objects
+    Extensions.activate!
+  end
+end
