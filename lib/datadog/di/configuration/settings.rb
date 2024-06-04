676f238f67ae68d2df330e5ad7fbfe72a6336ef3
Oleg Pudeyev <code@olegp.name>
2024-06-04 17:27:48 -0400

    note var names

--- lib/datadog/di/configuration/settings.rb
+++ lib/datadog/di/configuration/settings.rb
@@ -14,9 +14,16 @@ module Datadog
         # rubocop:disable Metrics/AbcSize,Metrics/MethodLength,Metrics/BlockLength,Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
         def self.add_settings!(base)
           base.class_eval do
+            # The setting has "internal" prefix to prevent it from being
+            # prematurely turned on by customers.
             settings :internal_dynamic_instrumentation do
               option :enabled do |o|
                 o.type :bool
+                # The environment variable has an "internal" prefix so that
+                # any customers that have the "proper" environment variable
+                # turned on (i.e. DD_DYNAMIC_INSTRUMENTATION_ENABLED)
+                # do not enable Ruby DI until the latter is ready for
+                # customer testing.
                 o.env 'DD_INTERNAL_DYNAMIC_INSTRUMENTATION_ENABLED'
                 o.default false
               end
