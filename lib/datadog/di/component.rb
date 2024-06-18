d28eecf293683123110c6c2c96ef046eee1bbd98
Oleg Pudeyev <code@olegp.name>
2024-06-18 15:50:53 -0400

    di obsolete

--- lib/datadog/di/component.rb
+++ lib/datadog/di/component.rb
@@ -35,21 +35,6 @@ module Datadog
 
       def shutdown!(replacement = nil)
       end
-
-      def add_probe_from_remote(config)
-        where = config.fetch('where')
-        if where.key?('sourceFile')
-          file = where.fetch('sourceFile')
-          lines = where.fetch('lines')
-
-          lines.each do |line|
-            line = Integer(line)
-            hook_manager.hook_line(file, line) do
-              puts 'hook executed'
-            end
-          end
-        end
-      end
     end
   end
 end
