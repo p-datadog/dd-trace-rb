014d50a84727aebf52de6b66152cdabcf92719d3
Oleg Pudeyev <code@olegp.name>
2024-07-02 13:38:51 -0400

    di connectivity

--- lib/datadog/di/probe_snapshot_client.rb
+++ lib/datadog/di/probe_snapshot_client.rb
@@ -2,9 +2,9 @@ module Datadog
   module DI
     # @api private
     class ProbeSnapshotClient
-      def initialize
-        uri = URI("http://localhost:8126/debugger/v1/input")
-        @client = Net::HTTP.new(uri.host, uri.port)
+      def initialize(agent_settings)
+        #uri = URI("http://localhost:8126/debugger/v1/input")
+        @client = Net::HTTP.new(agent_settings.hostname, uri.port)
         #http = Datadog::Core::Transport::HTTP::Adapters::Net.new(agent_settings)
       end
 
