94fd8c3d7a02df2c3c8abcef5d5515491e4e2dd4
Oleg Pudeyev <code@olegp.name>
2024-07-06 13:30:56 -0400

    di fix tests

--- spec/datadog/di/component_spec.rb
+++ spec/datadog/di/component_spec.rb
@@ -15,6 +15,13 @@ RSpec.describe Datadog::DI::Component do
     context 'when dynamic instrumentation is enabled' do
       let(:dynamic_instrumentation_enabled) { true }
 
+      before do
+        allow(agent_settings).to receive(:hostname)
+        allow(agent_settings).to receive(:port)
+        allow(agent_settings).to receive(:timeout_seconds).and_return(1)
+        allow(agent_settings).to receive(:ssl)
+      end
+
       it 'returns a Datadog::DI::Component instance' do
         component = described_class.build(settings, agent_settings)
         expect(component).to be_a(described_class)
