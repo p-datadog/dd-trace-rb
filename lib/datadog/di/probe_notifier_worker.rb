bc184c95e054f998e5ad7638673ecfca3f5889a9
Oleg Pudeyev <code@olegp.name>
2024-07-07 01:15:39 -0400

    di threads

--- lib/datadog/di/probe_notifier_worker.rb
+++ lib/datadog/di/probe_notifier_worker.rb
@@ -7,14 +7,36 @@ module Datadog
         @snapshot_queue = Queue.new
         @status_client = ProbeStatusClient.new(agent_settings)
         @snapshot_client = ProbeSnapshotClient.new(agent_settings)
+        @wake = ConditionVariable.new
       end
 
       def add_status(status)
         status_queue << status
+        wake.signal
       end
 
       def add_snapshot
         snapshot_queue << snapshot
+        wake.signal
+      end
+
+      def start
+        return if defined?(@thread)
+        @thread = Thread.new do
+          loop do
+            if maybe_send
+              # Run next iteration immediately in case more work is
+              # in the queue
+            else
+              sleep 1
+            end
+          end
+        end
+      end
+
+      def stop(timeout = 1)
+        wake.signal
+        wake.join(timeout)
       end
 
       private
@@ -23,13 +45,14 @@ module Datadog
       attr_reader :snapshot_queue
       attr_reader :status_client
       attr_reader :snapshot_client
+      attr_reader :wake
 
       DIAGNOSTICS_PATH = '/debugger/v1/diagnostics'
       INPUT_PATH = '/debugger/v1/input'
 
       def maybe_send
-        maybe_send_statuses
-        maybe_send_snapshots
+        rv = maybe_send_statuses
+        rv || maybe_send_snapshots
       end
 
       def maybe_send_statuses
@@ -45,6 +68,7 @@ module Datadog
             puts "failed to send probe statuses"
           end
         end
+        statuses.any?
       end
 
       def maybe_send_snapshots
@@ -60,15 +84,7 @@ module Datadog
             puts "failed to send probe snapshots"
           end
         end
-      end
-
-      def run
-        Thread.new do
-          loop do
-            maybe_send
-            sleep 1
-          end
-        end
+        snapshots.any?
       end
     end
   end
