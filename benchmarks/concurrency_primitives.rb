14c26ee1488332965cfc88f34dc875921227ef4a
Oleg Pudeyev <code@olegp.name>
2024-07-13 02:09:35 -0400

    di primitive bench

--- benchmarks/concurrency_primitives.rb
+++ benchmarks/concurrency_primitives.rb
@@ -52,10 +52,10 @@ class ConcurrencyPrimitivesBenchmark
         suite: report_to_dogstatsd_if_enabled_via_environment_variable(benchmark_name: 'di_instrument')
       )
 
-      hash = Concurrent::Map.new(a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 9, i: 10)
+      map = Concurrent::Map.new(a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 9, i: 10)
 
       x.report('Concurrent::Map access') do
-        hash[:a]
+        map[:a]
       end
 
       x.save! 'concurrency-primitives-results.json' unless VALIDATE_BENCHMARK_MODE
