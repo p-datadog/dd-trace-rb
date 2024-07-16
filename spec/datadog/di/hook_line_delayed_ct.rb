190a9b35a30a970bd1a2ee7b6ec01eef90d1b58e
Oleg Pudeyev <code@olegp.name>
2024-07-16 08:45:49 -0400

    di delayed wip

--- spec/datadog/di/hook_line_delayed_ct.rb
+++ spec/datadog/di/hook_line_delayed_ct.rb
@@ -1,9 +1,9 @@
-class HookLineTargetedTestClass
+class HookLineDelayedCtTestClass
   def test_method       # Line 2
     42                  # Line 3
   end
 end
 
 unless (actual = File.read(__FILE__).count("\n")) == 9
-  raise "Wrong number of lines in hook_line_targeted.rb: actual #{actual}, expected 9"
+  raise "Wrong number of lines in hook_line_delayed_ct.rb: actual #{actual}, expected 9"
 end
