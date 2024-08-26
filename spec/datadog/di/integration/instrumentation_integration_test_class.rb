9b6188db5ae84988cdb8c01f341c16f1ab883199
Oleg Pudeyev <code@olegp.name>
2024-08-26 17:05:40 -0400

    di test

--- spec/datadog/di/integration/instrumentation_integration_test_class.rb
+++ spec/datadog/di/integration/instrumentation_integration_test_class.rb
@@ -1,6 +1,12 @@
 class InstrumentationIntegrationTestClass
   def test_method
     a = 21
-    a * 2 # line 4
+    # padding
+    # padding
+    # padding
+    # padding
+    # padding
+    # padding
+    a * 2 # line 10
   end
 end
--- spec/datadog/di/integration/instrumentation_spec.rb
+++ spec/datadog/di/integration/instrumentation_spec.rb
@@ -52,35 +52,81 @@ RSpec.describe 'Instrumentation integration' do
     end
 
     context 'line probe' do
-      let(:probe_rc_spec) do
-         {"id"=>"3ecfd456-2d7c-4359-a51f-d4cc44141ffe",
-          "version"=>0,
-          "type"=>"LOG_PROBE",
-          "language"=>"ruby",
-          "where"=>{"sourceFile"=>"instrumentation_integration_test_class.rb", "lines"=>['4']},
-          "tags"=>[],
-          "template"=>"In aaa, line 1",
-          "segments"=>[{"str"=>"In aaa, line 1"}],
-          "captureSnapshot"=>false,
-          "capture"=>{"maxReferenceDepth"=>3},
-          "sampling"=>{"snapshotsPerSecond"=>5000},
-          "evaluateAt"=>"EXIT"}
-      end
-
-      it 'invokes probe' do
-        remote_processor.process(probe_rc_spec)
-        expect(Datadog::DI.component.probe_notifier_worker).to receive(:add_snapshot).once.and_call_original
-        expect(InstrumentationIntegrationTestClass.new.test_method).to eq(42)
-      end
-
-      it 'assembles expected notification payload' do
-        remote_processor.process(probe_rc_spec)
-        payload = nil
-        expect(Datadog::DI.component.probe_notifier_worker).to receive(:add_snapshot) do |_payload|
-          payload = _payload
+      context 'simple log probe' do
+        let(:probe_rc_spec) do
+           {"id"=>"3ecfd456-2d7c-4359-a51f-d4cc44141ffe",
+            "version"=>0,
+            "type"=>"LOG_PROBE",
+            "language"=>"ruby",
+            "where"=>{"sourceFile"=>"instrumentation_integration_test_class.rb", "lines"=>['10']},
+            "tags"=>[],
+            "template"=>"In aaa, line 1",
+            "segments"=>[{"str"=>"In aaa, line 1"}],
+            "captureSnapshot"=>false,
+            "capture"=>{"maxReferenceDepth"=>3},
+            "sampling"=>{"snapshotsPerSecond"=>5000},
+            "evaluateAt"=>"EXIT"}
         end
-        expect(InstrumentationIntegrationTestClass.new.test_method).to eq(42)
-        expect(payload).to be_a(Hash)
+
+        it 'invokes probe' do
+          remote_processor.process(probe_rc_spec)
+          expect(Datadog::DI.component.probe_notifier_worker).to receive(:add_snapshot).once.and_call_original
+          expect(InstrumentationIntegrationTestClass.new.test_method).to eq(42)
+        end
+
+        it 'assembles expected notification payload' do
+          remote_processor.process(probe_rc_spec)
+          payload = nil
+          expect(Datadog::DI.component.probe_notifier_worker).to receive(:add_snapshot) do |_payload|
+            payload = _payload
+          end
+          expect(InstrumentationIntegrationTestClass.new.test_method).to eq(42)
+          expect(payload).to be_a(Hash)
+          snapshot = payload.fetch(:'debugger.snapshot')
+          expect(snapshot).not_to have_key(:captures)
+          expect(snapshot).not_to have_key('captures')
+        end
+      end
+
+      context 'enriched probe' do
+        let(:probe_rc_spec) do
+           {"id"=>"3ecfd456-2d7c-4359-a51f-d4cc44141ffe",
+            "version"=>0,
+            "type"=>"LOG_PROBE",
+            "language"=>"ruby",
+            "where"=>{"sourceFile"=>"instrumentation_integration_test_class.rb", "lines"=>['10']},
+            "tags"=>[],
+            "template"=>"In aaa, line 1",
+            "segments"=>[{"str"=>"In aaa, line 1"}],
+            "captureSnapshot"=>true,
+            "capture"=>{"maxReferenceDepth"=>3},
+            "sampling"=>{"snapshotsPerSecond"=>5000},
+            "evaluateAt"=>"EXIT"}
+        end
+
+        let(:expected_captures) do
+          {lines: {10 => {locals: {
+            a: {},
+          }}}}
+        end
+
+        it 'invokes probe' do
+          remote_processor.process(probe_rc_spec)
+          expect(Datadog::DI.component.probe_notifier_worker).to receive(:add_snapshot).once.and_call_original
+          expect(InstrumentationIntegrationTestClass.new.test_method).to eq(42)
+        end
+
+        it 'assembles expected notification payload' do
+          remote_processor.process(probe_rc_spec)
+          payload = nil
+          expect(Datadog::DI.component.probe_notifier_worker).to receive(:add_snapshot) do |_payload|
+            payload = _payload
+          end
+          expect(InstrumentationIntegrationTestClass.new.test_method).to eq(42)
+          expect(payload).to be_a(Hash)
+          captures = payload.fetch(:'debugger.snapshot').fetch(:captures)
+          expect(captures).to eq(expected_captures)
+        end
       end
     end
   end
