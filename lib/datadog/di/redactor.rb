63998e76d40fa565e1c28c8da0d90b3ccd6be383
Oleg Pudeyev <code@olegp.name>
2024-09-10 09:16:47 -0400

    di redactor

---
+++ lib/datadog/di/redactor.rb
@@ -0,0 +1,151 @@
+# frozen_string_literal: true
+
+module Datadog
+  module DI
+    #
+    # @api private
+    class Redactor
+      def initialize(settings)
+        @settings = settings
+      end
+
+      attr_reader :settings
+
+      def redact_identifier?(name)
+        redacted_identifiers.include?(normalize(name))
+      end
+
+      def redact_type?(value)
+        # Classses can be nameless, do not attempt to redact in that case.
+        if cls_name = value.class.name
+          redacted_type_names_regexp.match?(cls_name)
+        else
+          false
+        end
+      end
+
+      private
+
+      def redacted_identifiers
+        @redacted_identifiers ||= begin
+          names = DEFAULT_REDACTED_IDENTIFIERS + settings.dynamic_instrumentation.redacted_identifiers
+          names.map! do |name|
+            normalize(name)
+          end
+          Set.new(names)
+        end
+      end
+
+      def redacted_type_names_regexp
+        @redacted_type_names_regexp ||= begin
+          names = settings.dynamic_instrumentation.redacted_type_names
+          names = names.map do |name|
+            if name.end_with?('*')
+              name = name[..-2]
+              suffix = '.*'
+            else
+              suffix = ''
+            end
+            Regexp.escape(name) + suffix
+          end.join('|')
+          Regexp.new("\\A(?:#{names})\\z")
+        end
+      end
+
+      # Copied from dd-trace-py
+      DEFAULT_REDACTED_IDENTIFIERS = [
+        "2fa",
+        "accesstoken",
+        "aiohttpsession",
+        "apikey",
+        "apisecret",
+        "apisignature",
+        "appkey",
+        "applicationkey",
+        "auth",
+        "authorization",
+        "authtoken",
+        "ccnumber",
+        "certificatepin",
+        "cipher",
+        "clientid",
+        "clientsecret",
+        "connectionstring",
+        "connectsid",
+        "cookie",
+        "credentials",
+        "creditcard",
+        "csrf",
+        "csrftoken",
+        "cvv",
+        "databaseurl",
+        "dburl",
+        "encryptionkey",
+        "encryptionkeyid",
+        "env",
+        "geolocation",
+        "gpgkey",
+        "ipaddress",
+        "jti",
+        "jwt",
+        "licensekey",
+        "masterkey",
+        "mysqlpwd",
+        "nonce",
+        "oauth",
+        "oauthtoken",
+        "otp",
+        "passhash",
+        "passwd",
+        "password",
+        "passwordb",
+        "pemfile",
+        "pgpkey",
+        "phpsessid",
+        "pin",
+        "pincode",
+        "pkcs8",
+        "privatekey",
+        "publickey",
+        "pwd",
+        "recaptchakey",
+        "refreshtoken",
+        "routingnumber",
+        "salt",
+        "secret",
+        "secretkey",
+        "secrettoken",
+        "securityanswer",
+        "securitycode",
+        "securityquestion",
+        "serviceaccountcredentials",
+        "session",
+        "sessionid",
+        "sessionkey",
+        "setcookie",
+        "signature",
+        "signaturekey",
+        "sshkey",
+        "ssn",
+        "symfony",
+        "token",
+        "transactionid",
+        "twiliotoken",
+        "usersession",
+        "voterid",
+        "xapikey",
+        "xauthtoken",
+        "xcsrftoken",
+        "xforwardedfor",
+        "xrealip",
+        "xsrf",
+        "xsrftoken",
+      ]
+
+      # Input can be a string or a symbol.
+      def normalize(str)
+        str.to_s.strip.downcase.gsub(/[-_$@]/, '')
+      end
+    end
+  end
+end
---
+++ spec/datadog/di/redactor_spec.rb
@@ -0,0 +1,77 @@
+require 'datadog/di/redactor'
+
+RSpec.describe Datadog::DI::Redactor do
+  let(:settings) do
+    double('settings').tap do |settings|
+      allow(settings).to receive(:dynamic_instrumentation).and_return(di_settings)
+    end
+  end
+
+  let(:di_settings) do
+    double('di settings').tap do |settings|
+      allow(settings).to receive(:enabled).and_return(true)
+      allow(settings).to receive(:propagate_all_exceptions).and_return(false)
+      allow(settings).to receive(:redacted_identifiers).and_return([])
+    end
+  end
+
+  let(:redactor) do
+    Datadog::DI::Redactor.new(settings)
+  end
+
+  describe '#redact_identifier?' do
+    CASES = [
+      ['lowercase', 'password', true],
+      ['uppercase', 'PASSWORD', true],
+      ['with removed punctiation', 'pass_word', true],
+      ['with non-removed punctuation', 'pass/word', false],
+    ]
+
+    CASES.each do |(label, identifier_, redact_)|
+      identifier, redact = identifier_, redact_
+
+      context label do
+        let(:identifier) { identifier }
+
+        it do
+          expect(redactor.redact_identifier?(identifier)).to be redact
+        end
+      end
+    end
+  end
+
+  describe '#redact_type?' do
+    class SensitiveType; end
+
+    let(:redacted_type_names) { %w[SensitiveType WildCard*] }
+
+    class WildCard; end
+    class WildCardClass; end
+    class WildCa; end
+
+    before do
+      expect(di_settings).to receive(:redacted_type_names).and_return(redacted_type_names)
+    end
+
+    CASES = [
+      ['redacted', SensitiveType.new, true],
+      ['not redacted', /123/, false],
+      ['primitive type', nil, false],
+      ['wild card type whose name is the same as prefix', WildCard.new, true],
+      ['wild card type', WildCardClass.new, true],
+      ['partial wild card prefix match', WildCa.new, false],
+    ]
+
+    CASES.each do |(label, value_, redact_)|
+      value, redact = value_, redact_
+
+      context label do
+        let(:value) { value }
+
+        it do
+          expect(redactor.redact_type?(value)).to be redact
+        end
+      end
+    end
+  end
+end
