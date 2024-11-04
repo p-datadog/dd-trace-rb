22b94e217a7856262a7eea92fa4019cad8aeeb2d
Oleg Pudeyev <code@olegp.name>
2024-11-04 11:33:27 -0500

    enable remaining DI benchmarks

--- benchmarks/di_instrument.rb
+++ benchmarks/di_instrument.rb
@@ -130,7 +130,6 @@ class DIInstrumentBenchmark
 
     instrumenter.unhook(probe)
 
-=begin Line probes require more of DI code to be merged
     calls = 0
     probe = Datadog::DI::Probe.new(id: 1, type: :log,
       file: file, line_no: line + 1)
@@ -206,7 +205,6 @@ class DIInstrumentBenchmark
     # target code is approximately what it was prior to hook installation.
 
     instrumenter.unhook(probe)
-=end
 
     calls = 0
 
