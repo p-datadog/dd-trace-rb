d2371ffd2fd28127ee09769f0b58bbafbe73988a
Oleg Pudeyev <code@olegp.name>
2025-02-21 17:26:19 -0500

    di

--- lib/datadog/di/transport/http/diagnostics.rb
+++ lib/datadog/di/transport/http/diagnostics.rb
@@ -60,6 +60,22 @@ module Datadog
                 super(:post, path)
                 @encoder = encoder
               end
+
+              def call(env, &block)
+                # Add trace count header
+                #env.headers[HEADER_TRACE_COUNT] = env.request.parcel.trace_count.to_s
+
+                # Encode body & type
+                require'byebug';byebug
+                #env.headers[HEADER_CONTENT_TYPE] = encoder.content_type
+                #env.body = env.request.parcel.data
+                event_payload = Core::Vendor::Multipart::Post::UploadIO.new(
+                  StringIO.new(JSON.dump(env.request.parcel.data)), 'application/json', 'event.json'
+                )
+                env.form = {'event' => event_payload}
+
+                super(env, &block)
+              end
             end
           end
         end
