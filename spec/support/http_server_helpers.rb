29a7bbf3ee49f09fb996a0c1b71423e1ae2d259d
Oleg Pudeyev <code@olegp.name>
2025-04-10 16:06:33 -0400

    implement differently

--- spec/support/http_server_helpers.rb
+++ spec/support/http_server_helpers.rb
@@ -2,7 +2,7 @@ require 'webrick'
 
 module HttpServerHelpers
   module ClassMethods
-    def http_server(&block)
+    def define_http_server(&block)
       let(:http_server_port) { http_server[:Port] }
       let(:http_server_init_signal) { Queue.new }
       let(:http_server_options) { {} }
@@ -15,37 +15,25 @@ module HttpServerHelpers
           instance_exec(http_server, &block)
         end
       end
+    end
+
+    def http_server(&block)
+      define_http_server(&block)
 
       around do |example|
-        # If there are multiple http_server calls in an example
-        # (for example, some are from an outer context),
-        # the above let blocks would be replaced but the around hook
-        # will be invoked once for each http_server call whereas the server
-        # startup and shutdown only needs to be executed once per example.
-        # Track whether the hook was invoked already and skip startup and
-        # shutdown on the inner invocations, but still invoke the example
-        # itself from each hook invocation.
-        do_cleanup = false
-        unless @http_server_hook_executed
-          @http_server_hook_executed = true
-          do_cleanup = true
-
-          @server_thread = Thread.new do
-            http_server.start
-          rescue Exception
-            http_server_init_signal.push(1)
-            raise
-          end
-          http_server_init_signal.pop
-          expect(@server_thread).to be_alive
+        @server_thread = Thread.new do
+          http_server.start
+        rescue Exception
+          http_server_init_signal.push(1)
+          raise
         end
+        http_server_init_signal.pop
+        expect(@server_thread).to be_alive
 
         example.run
 
-        if do_cleanup
-          http_server.shutdown
-          @server_thread.join
-        end
+        http_server.shutdown
+        @server_thread.join
       end
     end
   end
