38d97dd5d3f5b7b45b5bb256ec438975bb670201
Oleg Pudeyev <code@olegp.name>
2025-04-10 15:36:37 -0400

    crashtracker

--- spec/support/http_server_helpers.rb
+++ spec/support/http_server_helpers.rb
@@ -2,33 +2,35 @@ require 'webrick'
 
 module HttpServerHelpers
   module ClassMethods
-    def http_server(&block)
+    def http_server(append: false, &block)
       let(:http_server_port) { http_server[:Port] }
       let(:http_server_init_signal) { Queue.new }
+      let(:http_server_options) { {} }
 
       let(:http_server) do
-        WEBrick::HTTPServer.new(
+        WEBrick::HTTPServer.new({
           Port: 0,
           StartCallback: -> { http_server_init_signal.push(1) }
-        ).tap do |http_server|
-          instance_exec(http_server,&block)
+        }.merge(http_server_options)).tap do |http_server|
+          instance_exec(http_server, &block)
         end
       end
 
       around do |example|
-        @server_thread = Thread.new do
-          http_server.start
-        rescue Exception
-          http_server_init_signal.push(1)
-          raise
+        unless append
+          @server_thread = Thread.new do
+            http_server.start
+          rescue Exception
+            http_server_init_signal.push(1)
+            raise
+          end
+          http_server_init_signal.pop
+          expect(@server_thread).to be_alive
         end
-        http_server_init_signal.pop
-        expect(@server_thread).to be_alive
         example.run
-        @server_thread.kill
-        loop do
-          break unless @server_thread.alive?
-          sleep 0.5
+        unless append
+          http_server.shutdown
+          @server_thread.join
         end
       end
     end
